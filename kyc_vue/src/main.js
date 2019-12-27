cb445bab885dd1542d334fc22d871b773fe2322c
Rahul Trivedi <rahultrivedi180@gmail.com>
2019-12-27 23:56:13 +0530

    vue connected dapp

--- kyc_vue/src/main.js
+++ kyc_vue/src/main.js
@@ -3,11 +3,13 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import './registerServiceWorker'
+import EmbarkJS from '../embarkArtifacts/embarkjs'
 
 Vue.config.productionTip = false
 
 new Vue({
   router,
   store,
+  EmbarkJS,
   render: h => h(App)
 }).$mount('#app')
