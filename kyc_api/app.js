4afd090bf45ca96bf7069af9a1b9890fcfd86177
Rahul Trivedi <rahultrivedi180@gmail.com>
2019-12-28 11:32:02 +0530

    Express app init

---
+++ kyc_api/app.js
@@ -0,0 +1,34 @@
+require("dotenv").config();
+const compression = require("compression");
+const express = require("express");
+const helmet = require("helmet");
+const cookieParser = require("cookie-parser");
+const morgan = require("morgan");
+
+const app = express();
+
+app
+  .use(helmet())
+  .use(compression())
+  .use(morgan("dev"))
+  .use(express.json())
+  .use(express.urlencoded({ extended: true }))
+  .use(cookieParser());
+
+app.use((req, res, next) => {
+  return res.status(404).json({ err: "Route Not Found", msg: "" });
+});
+
+if (app.get("env") === "development") {
+  app.use((err, req, res, next) => {
+    return res.status(err.status || 500).json({ err: err, msg: err.msg });
+  });
+}
+
+if (app.get("env") === "production") {
+  app.use((err, req, res, next) => {
+    return res.status(err.status || 500).json({ err: "Something Went Wrong", msg: "" });
+  });
+}
+
+module.exports = app;
---
+++ kyc_api/server.js
@@ -0,0 +1,47 @@
+require("dotenv").config();
+const http = require("http");
+const app = require("./app");
+
+const server = http.createServer(app);
+
+const normalizePort = val => {
+  const port = parseInt(val, 10);
+  if (port >= 0) {
+    return port;
+  }
+  return false;
+};
+
+const port = normalizePort(process.env.PORT || "3000");
+
+const onError = error => {
+  if (error.syscall !== "listen") {
+    throw error;
+  }
+
+  const bind = typeof port === "string" ? `Pipe ${port}` : `Port ${port}`;
+
+  switch (error.code) {
+    case "EACCES":
+      console.error(`${bind} requires elevated privileges`);
+      process.exit(1);
+      break;
+    case "EADDRINUSE":
+      console.error(`${bind} is already in use`);
+      process.exit(1);
+      break;
+    default:
+      throw error;
+  }
+};
+
+const onListening = () => {
+  const addr = server.address();
+  const bind = typeof addr === "string" ? `pipe ${addr}` : `port ${addr.port}`;
+  console.log(`Listening on ${bind}`);
+};
+
+app.set("port", port);
+server.listen(port);
+server.on("error", onError);
+server.on("listening", onListening);
